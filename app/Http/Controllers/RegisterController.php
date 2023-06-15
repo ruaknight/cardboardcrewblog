8a752ca53d8ff5a6a00373953fb228badc06bde1
ruaknight <ruaknight@gmail.com>
2023-06-15 17:38:41 +0700

    Add register form

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    //
+    public function create() {
+        return view('register.create');
+    }
+
+    public function store() {
+        $attributes = request()->validate([
+            'name' => ['required', 'max:255', 'unique:users,name', 'min:3'],
+            'email' => ['required', 'email', 'max:255', 'unique:users,email'],
+            'password' => ['required', 'min:7', 'max:255' ]
+        ]);
+
+        $attributes['role'] = 'author';
+
+        User::create($attributes);
+
+        session()->flash('success', 'account created');
+
+
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\Tag;
@@ -34,5 +35,8 @@ Route::get('/authors/{author:name}', function (User $author) {
 });
 
 Route::get('/post/{post:title}', [PostController::class, 'show']);
+
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
 
 
